8bb01f390012d7f049129587325c5a1906771918
Matias Corpu <164563483+MatiasCorpu@users.noreply.github.com>
2025-01-14 19:21:43 -0300

    update project

--- src/pages/CurriculumVitae/CurriculumVitae.jsx
+++ src/pages/CurriculumVitae/CurriculumVitae.jsx
@@ -4,8 +4,9 @@ import ButtonDownload from "./components/ButtonDownload";
 function CurriculumVitae() {
   return (
     <div className="relative w-full flex justify-center items-center">
-      <img src="src\assets\cv.png" alt="Curriculum Vitae" />
-
+      {/* <img src="src\assets\cv.png" alt="Curriculum Vitae" /> */}
+      <div className="w-[1000px] h-[1000px] bg-green-700/20"></div>
+ 
       <ButtonDownload />
     </div>
   );
--- src/pages/CurriculumVitae/components/ButtonDownload.jsx
+++ src/pages/CurriculumVitae/components/ButtonDownload.jsx
@@ -1,15 +1,18 @@
-import { button } from "framer-motion/client";
 import React from "react";
+import { IconDownload } from "@tabler/icons-react";
+import { button } from "framer-motion/client";
 
 function ButtonDownload() {
   return (
-    <button>
+    <button className="h-14 w-14 fixed bottom-24 right-24 flex items-center justify-center rounded-full bg-green-700 transition-transform duration-300 group hover:scale-110">
+
+      <span className="sr-only">Download Curriculum Vitae</span>
       <a
         href="src\assets\cv.pdf"
         download="CurriculumVitae.pdf"
-        className=" bg-red-700 fixed bottom-24 right-2 rounded-full p-1"
+        className=""
       >
-        <span className="">Download</span>
+        <IconDownload />
       </a>
     </button>
   );
